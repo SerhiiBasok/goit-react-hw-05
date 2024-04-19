f60bc4b40bcf00f22c5af42861ba32484b11dfe2
SerhiiBasok <sergeybsk19@gmail.com>
2024-04-19 22:57:32 +0300

    all elem

--- goit-react-hw-05/src/components/MovieReviews/MovieReviews.jsx
+++ goit-react-hw-05/src/components/MovieReviews/MovieReviews.jsx
@@ -1,4 +1,47 @@
-function MovieReviews() {
-  return <div>Reviews</div>;
-}
+import { useState, useEffect } from "react";
+import { fetchMovieReviews } from "../MoviesApi/MoviesApi";
+import { useParams } from "react-router-dom";
+
+const MovieReviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const reviewsData = await fetchMovieReviews(movieId);
+        setReviews(reviewsData);
+      } catch (error) {
+        console.error("Error fetching movie reviews:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchReviews();
+  }, [movieId]);
+
+  if (loading) {
+    return <p>Loading reviews...</p>;
+  }
+
+  return (
+    <div>
+      <h2>Movie Reviews</h2>
+      {reviews.length > 0 ? (
+        <ul>
+          {reviews.map((review) => (
+            <li key={review.id}>
+              <h3>{review.author}</h3>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No reviews available</p>
+      )}
+    </div>
+  );
+};
+
 export default MovieReviews;
--- goit-react-hw-05/src/components/Navigation/Navigation.jsx
+++ goit-react-hw-05/src/components/Navigation/Navigation.jsx
@@ -1,15 +1,25 @@
 import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import css from "./Navigation.module.css";
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 const Navigation = () => {
   return (
-    <header>
+    <header className={css.header}>
       <nav>
-        <ul>
+        <ul className={css.list}>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/" className={buildLinkClass}>
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/movies">Movies</NavLink>
+            <NavLink to="/movies" className={buildLinkClass}>
+              Movies
+            </NavLink>
           </li>
         </ul>
       </nav>
